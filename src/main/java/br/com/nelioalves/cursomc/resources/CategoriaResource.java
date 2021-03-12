65fc0493e8e7c8217407690ca54f53daf33ff1b1
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-03-12 03:13:02 -0300

    Adicionado o dominio de Categoria e testando o Rest

--- src/main/java/br/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,33 @@
 package br.com.nelioalves.cursomc.resources;
 
+import br.com.nelioalves.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String Listar(){
-        return "Rest esta funcionando";
+    public List<Categoria> Listar(){
+        Categoria categoria1 = new Categoria(1, "Informatica");
+        Categoria categoria2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+
+        return lista;
     }
 
 
 
 }
+
+
+
+

880008db83810d0a5aacf26e12a8b1a48b8cbdef
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2021-03-12 02:45:39 -0300

    Testando o Rest

---
+++ src/main/java/br/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package br.com.nelioalves.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String Listar(){
+        return "Rest esta funcionando";
+    }
+
+
+
+}
